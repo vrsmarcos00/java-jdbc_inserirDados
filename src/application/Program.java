1365fb1713c0548e2c28eaa0c4e8ce938b67f504
vrsmarcos00 <vrr.marcos@gmail.com>
2021-10-06 22:40:32 -0300

    Simple insertion with prepared statement

---
+++ src/application/Program.java
@@ -0,0 +1,45 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+		Connection conn = null;
+		PreparedStatement st = null;
+
+		try {
+			conn = DB.getConnection();
+
+			st = conn.prepareStatement("INSERT INTO seller" + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES " + "(?, ?, ?, ?, ?)");
+
+			st.setString(1, "Marcos de Sá");
+			st.setString(2, "vrr.marcos@gmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("16/09/2000").getTime()));
+			st.setDouble(4, 2500.00);
+			st.setInt(5, 4);
+
+			int rowsAffected = st.executeUpdate();
+
+			System.out.println("Done! Rows affected: " + rowsAffected);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+	}
+
+}
